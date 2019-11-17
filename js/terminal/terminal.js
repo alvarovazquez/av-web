3a2fe0109b90bb52643ba8892743186b531604aa
Alvaro <contacto@alvarovazquez.es>
2019-11-17 21:25:49 +0100

    - Added support for different modes of input - Now cursor text changes when goint into reverse search mode (Ctrl + R)

--- js/terminal/terminal.js
+++ js/terminal/terminal.js
@@ -7,12 +7,23 @@ import {
 } from './commands/command-history.js';
 
 const INPUT_QUERY_SELECTOR = '.user-input';
+const FEEDBACK_QUERY_SELECTOR = '.user-feedback';
 const CURSOR_QUERY_SELECTOR = '.cursor';
 const CONTENT_QUERY_SELECTOR = '.content';
+const FEEDBACK_CLASS = 'user-feedback';
+const INPUT_MODE_COMMAND = 'INPUT_MODE_COMMAND';
+const INPUT_MODE_REVERSE = 'INPUT_MODE_REVERSE';
+const INPUT_MODE_REVERSE_FAILED = 'INPUT_MODE_REVERSE_FAILED';
+const INPUT_MODES_CLASSNAMES_MAP = {
+	[INPUT_MODE_COMMAND]: 'command',
+	[INPUT_MODE_REVERSE]: 'reverse-search',
+	[INPUT_MODE_REVERSE_FAILED]: 'reverse-search-failed'
+};
 
 let $terminal;
 let $cursor;
 let currentCursorPos = 0;
+let currentMode = INPUT_MODE_COMMAND;
 
 function setFocus() {
 	$terminal.querySelector(INPUT_QUERY_SELECTOR).focus();
@@ -50,12 +61,20 @@ function moveCursorRight() {
 	}
 }
 
+function changeMode(mode) {
+	let $userInput = document.querySelector(FEEDBACK_QUERY_SELECTOR);
+
+	currentMode = mode;
+	$userInput.setAttribute('class', `${FEEDBACK_CLASS} ${INPUT_MODES_CLASSNAMES_MAP[mode]}`);
+}
+
 function resetInput() {
 	const $input = $terminal.querySelector(INPUT_QUERY_SELECTOR);
 
 	currentCursorPos = 0;
 	updateUserInputHtml();
 	$input.value = '';
+	changeMode(INPUT_MODE_COMMAND);
 }
 
 function emptyInput() {
@@ -71,9 +90,12 @@ function autoScroll() {
 
 function addUserFeddback() {
 	const $newUserInputFeedback = document.createElement('p');
+	const $currentUserInputFeedback = document.querySelector(FEEDBACK_QUERY_SELECTOR);
 	const $content = $terminal.querySelector(CONTENT_QUERY_SELECTOR);
 
 	$newUserInputFeedback.appendChild($cursor);
+	$newUserInputFeedback.setAttribute('class', `${FEEDBACK_CLASS} ${INPUT_MODES_CLASSNAMES_MAP[currentMode]}`);
+	$currentUserInputFeedback.setAttribute('class', INPUT_MODES_CLASSNAMES_MAP[INPUT_MODE_COMMAND]);
 	$content.appendChild($newUserInputFeedback);
 
 	autoScroll();
@@ -119,7 +141,9 @@ function initEvents() {
 
 		if (event.isComposing || event.keyCode === 229) {
 			return;
-		} else if (event.ctrlKey || event.altKey) {
+		} else if (event.key === 'r' && event.ctrlKey) {
+			event.preventDefault();
+			changeMode(INPUT_MODE_REVERSE);
 			return;
 		} else if (event.key === 'Enter') {
 			resetCommandHistory();
@@ -141,8 +165,12 @@ function initEvents() {
 
 			return;
 		} else if (event.key === 'Escape') {
-			resetCommandHistory();
-			emptyInput();
+			if (currentMode === INPUT_MODE_COMMAND) {
+				resetCommandHistory();
+				emptyInput();
+			} else if (currentMode === INPUT_MODE_REVERSE) {
+				resetInput();
+			}
 			return;
 		} else if (event.key === 'ArrowLeft') {
 			moveCursorLeft();
@@ -161,6 +189,8 @@ function initEvents() {
 				$userInput.value = commandInHistory;
 				updateUserInputHtml();
 			}
+			return;
+		} else if (event.ctrlKey || event.altKey) {
 			return;
 		}
 
